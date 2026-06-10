f73d06c85d1b9a47ac15384a41b12bba910c5ea7
Phal-viwattana404 <Phalviwattanafreelancer@gmail.com>
2026-06-10 12:12:40 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:mynotes/firebase_options.dart';
+
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color.fromARGB(255, 0, 170, 255),
+          foregroundColor: Color.fromARGB(255, 255, 255, 255),
+        ),
+      ),
+      home: const HomePage(), 
+    ),
+  );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login")
+      ),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+            final user = FirebaseAuth.instance.currentUser;
+            if(user?.emailVerified ?? false) {
+              print('You are verified');
+            }else{
+              print('You need to verify your Email');
+            }
+            return const Text('Done');
+              default:
+            return const Text('Loading...');
+          }
+        },
+      ),
+    );
+  }
+}
+
